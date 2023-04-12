8477b5d008773230735c34922663062541fb59ed
Nassim Bourebia <b.nassim01@gmail.com>
2023-04-12 17:03:38 +0200

    add js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("connecté");
+//Je sélectionne et je stocke
+
+//la DIV switch
+const switchBox = document.querySelector('.switch'); 
+
+//la DIV btn (le cercle)
+const btn = document.querySelector('.btn'); 
+
+//l'icone 
+const icone = document.querySelector('i'); 
+
+//la Div container 
+const container = document.querySelector('.container'); 
+
+//le titre 
+const titre = document.querySelector('h1')
+
+//Je soumets la Div switch à une action au clic
+switchBox.addEventListener('click', function(){
+    console.log('div cliqué');
+    btn.classList.toggle('btn-change')
+})
